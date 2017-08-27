9c2bbecab8f9a8551099f4a4792a9a156135b668
wusq121 <635219415@qq.com>
2017-08-27 22:38:57 +0800

    wu

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -10,7 +10,7 @@ public class LongestPalindromicSubstring {
 		}
 		return true;
 	}
-	public String LongestPalindrome(String s){
+	public String LongestPalindrome1(String s){
 		int l = 0;
 		String temp, res = null;
 		for(int i = 0; i < s.length(); i++){
@@ -26,12 +26,35 @@ public class LongestPalindromicSubstring {
 		}
 		return res;
 	}
+	private int expand(String s, int left, int right) {
+		int L = left, R = right;
+		while((L >= 0) && (R < s.length()) && (s.charAt(L) == s.charAt(R))){
+			L--;
+			R++;
+		}
+		return R - L - 1;
+	}
+	
+	public String LongestPalindrome2(String s){
+		int start = 0, end = 0; 
+		for(int i = 0; i < s.length(); i++){
+			int len1 = expand(s, i, i);
+			int len2 = expand(s, i, i + 1);
+			int len = Math.max(len1, len2);
+			if(len > (end - start)){
+				start = i - (len - 1)/2;
+				end = i + len/2;
+			}
+		}
+		return s.substring(start, end + 1);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LongestPalindromicSubstring l = new LongestPalindromicSubstring();
 		String s = "klvxwqyzugrdoaccdafdfrvxiowkcuedfhoixzipxrkzbvpusslsgfjocvidnpsnkqdfnnzzawzsslwnvvjyoignsfbxkgrokzyusxikxumrxlzzrnbtrixxfioormoyyejashrowjqqzifacecvoruwkuessttlexvdptuvodoavsjaepvrfvbdhumtuvxufzzyowiswokioyjtzzmevttheeyjqcldllxvjraeyflthntsmipaoyjixygbtbvbnnrmlwwkeikhnnmlfspjgmcxwbjyhomfjdcnogqjviggklplpznfwjydkxzjkoskvqvnxfzdrsmooyciwulvtlmvnjbbmffureoilszlonibbcwfsjzguxqrjwypwrskhrttvnqoqisdfuifqnabzbvyzgbxfvmcomneykfmycevnrcsyqclamfxskmsxreptpxqxqidvjbuduktnwwoztvkuebfdigmjqfuolqzvjincchlmbrxpqgguwuyhrdtwqkdlqidlxzqktgzktihvlwsbysjeykiwokyqaskjjngovbagspyspeghutyoeahhgynzsyaszlirmlekpboywqdliumihwnsnwjc";
-		System.out.println("longest:" + l.LongestPalindrome(s));
+		System.out.println("longest:" + l.LongestPalindrome1(s));
+		System.out.println("longest fast:" + l.LongestPalindrome2(s));
 	}
 
 }
